25062891e9ca9303bb0fde8460daeb655a577323
Daniel Mendes de Aguiar <daniel.mendes.aguiar18@gmail.com>
2024-08-14 12:03:32 -0300

    feat: Adicionado pagina home e sua estilização, instalado daisyui e tallstackui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+    ],
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/tallstackui/tallstackui/src/**/*.php',
     ],
 
     theme: {
@@ -19,5 +25,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, daisyui],
 };
