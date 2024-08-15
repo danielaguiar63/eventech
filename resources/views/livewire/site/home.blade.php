bcad01dc290ee9fdacf1f53f0636e23f4ac47681
Daniel Mendes de Aguiar <daniel.mendes.aguiar18@gmail.com>
2024-08-14 23:33:34 -0300

    feat: Adicionado componetes navbar e footer, logos, e pagina inicial

--- resources/views/livewire/site/home.blade.php
+++ resources/views/livewire/site/home.blade.php
@@ -1,15 +1,41 @@
-<div class="w-full h-screen">
-    <section class="flex w-full h-1/2 bg-blue-900">
-        <div class="flex">
-            <h1>Conectando sua comunidade a eventos imperdíveis: descubra o melhor que a nossa cidade tem a oferecer com Eventech!</h1>
-        </div>
-        <div>
-            <img src="https://via.placeholder.com/500" alt="">
+<div class="w-full">
+    <section class="overflow-hidden flex w-full bg-gray-900">
+        <div class="flex w-full items-center justify-center p-8 ">
+            <div class="z-10 flex flex-col space-y-2 font-roboto text-5xl text-white">
+                <span class="ml-10 text-6xl font-bold">Conectando sua comunidade</span>
+                <span class="ml-60 ">a eventos imperdíveis:</span>
+                <span class="ml-72 text-6xl font-bold ">descubra o melhor que a </span>
+                <span class="ml-48">nossa cidade tem a oferecer</span>
+                <h1 class="ml-28 font-bold text-7xl">com a Eventech!</h1>
+            </div>
+        <div class="w-1/2 ml-[-150px] flex items-center justify-center">
+                <img class="w-full" src="{{ asset('Images/svg/Events-cuate.svg') }}" alt="Imagem da home Eventos">
+            </div>
         </div>
     </section>
-
-    <section class="w-full h-1/2 bg-gray-600">
-        <h2></h2>
+    <section class="overflow-hidden flex w-full h-screen bg-gray-200 items-center justify-center p-8">
+        <div class="text-center">
+            <p>Conectando sua comunidade</p>
+            <p>a eventos imperdíveis</p>
+            <p>descubra o melhor que a nossa cidade tem a oferecer</p>
+            <h1>com Eventech!</h1>
+        </div>
+    </section>
+    <!-- Seção adicional, se necessário -->
+    <section class="overflow-hidden flex w-full h-screen bg-gray-800 items-center justify-center p-8">
+        <div class="text-center text-white">
+            <p>Conectando sua comunidade</p>
+            <p>a eventos imperdíveis</p>
+            <p>descubra o melhor que a nossa cidade tem a oferecer</p>
+            <h1>com Eventech!</h1>
+        </div>
+    </section>
+    <section class="overflow-hidden flex w-full h-screen bg-gray-200 items-center justify-center p-8">
+        <div class="text-center">
+            <p>Conectando sua comunidade</p>
+            <p>a eventos imperdíveis</p>
+            <p>descubra o melhor que a nossa cidade tem a oferecer</p>
+            <h1>com Eventech!</h1>
+        </div>
     </section>
 </div>
-
